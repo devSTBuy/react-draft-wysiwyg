f913a19185b8f983dcb045fe520166ad6c10dc11
FernandoMaccio <fernando.maccio@onetree.com>
2021-05-05 12:43:11 -0300

    no editor test

--- src/Editor/index.js
+++ src/Editor/index.js
@@ -483,23 +483,7 @@ class WysiwygEditor extends Component {
           onBlur={this.onEditorBlur}
           onKeyDown={KeyDownHandler.onKeyDown}
           onMouseDown={this.onEditorMouseDown}
-        >
-          <Editor
-            ref={this.setEditorReference}
-            keyBindingFn={this.keyBindingFn}
-            editorState={editorState}
-            onChange={this.onChange}
-            blockStyleFn={blockStyleFn}
-            customStyleMap={this.getStyleMap(this.props)}
-            handleReturn={this.handleReturn}
-            handlePastedText={this.handlePastedTextFn}
-            blockRendererFn={this.blockRendererFn}
-            handleKeyCommand={this.handleKeyCommand}
-            ariaLabel={ariaLabel || "rdw-editor"}
-            blockRenderMap={blockRenderMap}
-            {...this.editorProps}
-          />
-        </div>
+        ></div>
       </div>
     );
   }
